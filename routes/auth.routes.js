d984e0c178172362798977eb657616de9a3ebd63
kim <khemgabz@gmail.com>
2020-12-15 21:03:55 +0800

    firebase middleware

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,9 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
+// middlewares
+const { authCheck } = require("../middlewares/auth");
+
 // import
 const { createOrUpdateUser } = require("../controllers/auth.controller");
 
-router.get("/create-or-update-user", createOrUpdateUser);
+router.get("/create-or-update-user", authCheck, createOrUpdateUser);
 
 module.exports = router;
