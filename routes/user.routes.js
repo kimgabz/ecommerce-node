ea622935741bf9a657d5fb62f72f1172f73b1a42
kim <khemgabz@gmail.com>
2021-02-02 17:51:25 +0800

    cod

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -12,7 +12,11 @@ const {
     saveAddress,
     applyCouponToUserCart,
     createOrder,
-    orders
+    orders,
+    addToWishlist,
+    wishlist,
+    removeFromWishlist,
+    createCashOrder
 } = require("../controllers/user.controller");
 
 router.post("/user/cart", authCheck, userCart); // save cart
@@ -20,11 +24,17 @@ router.get("/user/cart", authCheck, getUserCart); // get cart
 router.delete("/user/cart", authCheck, emptyCart); // empty cart
 router.post("/user/address", authCheck, saveAddress);
 
-router.post("/user/order", authCheck, createOrder);
+router.post("/user/order", authCheck, createOrder); // stripe
+router.post("/user/cash-order", authCheck, createCashOrder); // cod
 router.get("/user/orders", authCheck, orders);
 
 // coupon
 router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
+
+// wishlist
+router.post("/user/wishlist", authCheck, addToWishlist);
+router.get("/user/wishlist", authCheck, wishlist);
+router.put("/user/wishlist/:productId", authCheck, removeFromWishlist);
 
 // router.get("/user", (req, res) => {
 //   res.json({
